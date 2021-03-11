e4506c68021b25b6644601f3d81bcf45b2359078
Ivan Kvyatkovskiy <ivan.kvyatkovski@gmail.com>
2021-03-11 07:49:47 +0700

    added solution

--- 2-32.scm
+++ 2-32.scm
@@ -2,9 +2,14 @@
 #lang scheme
 
 (define (subsets s)
+  (display s)
+  (newline)
   (if (null? s)
       (list '())
       (let ((rest (subsets (cdr s))))
-        (append rest (map subsets rest)))))
+        (append rest (map (lambda (x) (cons (car s) x))
+                          rest)))))
 
 (subsets (list 1 2 3))
+
+
