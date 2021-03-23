384e57566c9a265c1fba27640e434773ca4056bf
Ivan Kvyatkovskiy <ivan.kvyatkovski@gmail.com>
2021-03-23 16:43:36 +0700

    added solution

---
+++ 2-48.scm
@@ -0,0 +1,22 @@
+#lang sicp
+
+(#%require sicp-pict)
+
+(define (make-vect x y)
+  (cons x y))
+
+(define (make-segment v1 v2)
+  (list v1 v2))
+
+(define (start-segment v)
+  (car v))
+
+(define (end-segment v)
+  (cadr v))
+
+(define s (make-segment (make-vect 1 1)
+                        (make-vect 5 5)))
+
+s
+(start-segment s)
+(end-segment s)
