89a80a4a96ff99c82e3d30cd8e951b3b5f644e0e
Ivan Kvyatkovskiy <ivan.kvyatkovski@gmail.com>
2021-04-03 16:08:23 +0700

    added solution

---
+++ 2-64.scm
@@ -0,0 +1,30 @@
+#lang sicp
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree
+                                 (cdr non-left-elts)
+                                 right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree
+                       this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+(list->tree '(1 3 5 7 9 11))
+
+;Takes a list then gets median element of list and create a node with an entry as this median element, left branch is partial-tree called recursively but with half length, and because of the length it will also return right part (all the remaining elements) untouched as a cdr which will be used to create the right branch
