95f2bf3edfc99d3a6419ea0fbd4aa9562560980a
Ivan Kvyatkovskiy <ivan.kvyatkovski@gmail.com>
2021-04-07 21:36:30 +0700

    added solution

---
+++ 2-73.scm
@@ -0,0 +1,55 @@
+#lang scheme
+
+(require "modules/sicp/sicp.rkt")
+(require "dispatch-table.scm")
+(require "2-57.scm")
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        (else
+         ((get 'deriv (operator exp))
+          (operands exp)
+          var))))
+
+(define (deriv-sum exp var)
+   (make-sum (deriv (addend exp) var)
+             (deriv (augend exp) var)))
+
+(define (deriv-product exp var)
+   (make-sum
+    (make-product (multiplier exp)
+                  (deriv (multiplicand exp) var))
+    (make-product (deriv (multiplier exp) var)
+                  (multiplicand exp))))
+
+(define (deriv-exp exp var)
+   (make-product  
+    (exponent exp) 
+    (make-exponentiation (base exp) 
+                         (make-sum (exponent exp) -1))))
+
+(define (operator exp) (car exp))
+(define (operands exp) exp)
+
+; a. number and variable are not tagged with type
+; variable is a symbol but whether it's a type or not depends on the argument
+
+; b.
+
+(put 'deriv '+ deriv-sum)
+(put 'deriv '* deriv-product)
+
+; c.
+(put 'deriv '** deriv-exp)
+
+(deriv '(* x x) 'x)
+(deriv '(* x y) 'x)
+
+;d. Only change to get procedure to swap aruments is required 
