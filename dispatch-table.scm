a16aae967bea87a511b638254d6c53bd347bd397
Ivan Kvytakovskiy <ivan.kvyatkovski@gmail.com>
2021-04-10 16:54:21 +0700

    updates dispatch table

--- dispatch-table.scm
+++ dispatch-table.scm
@@ -1,7 +1,13 @@
 #lang scheme
 
+(require "modules/sicp/sicp.rkt")
 (provide get
-         put)
+         put
+         apply-generic
+         attach-tag
+         type-tag
+         contents
+         dispatch-table)
 
 (define dispatch-table '())
 
@@ -15,9 +21,36 @@
 (define (dispatch-proc row) (caddr row))
 
 (define (get op type)
-  (dispatch-proc (car (filter (lambda (row)
+  (let ((rows (filter (lambda (row)
                                 (and (eq? op (dispatch-op row))
-                                     (eq? type (dispatch-type row))))
-                              dispatch-table))))
+                                     (equal? type (dispatch-type row))))
+                              dispatch-table)))
+    (if (pair? rows)
+        (dispatch-proc (car rows))
+        nil)))
 
+(define (attach-tag tag contents)
+  (if (eq? tag 'scheme-number)
+      contents
+      (cons tag contents)))
 
+(define (type-tag datum)
+  (if (number? datum)
+      'scheme-number
+      (if (pair? datum)
+          (car datum)
+          (error "Bad tagged datum - TYPE-TAG" datum))))
+
+(define (contents datum)
+  (if (number? datum)
+      datum
+      (if (pair? datum)
+          (cdr datum)
+          (error "Bad tagged datum - CONTENTS" datum))))
+
+(define (apply-generic op . args)
+  (let ((type-tags (flatmap type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if (not (null? proc))
+          (apply proc (map contents args))
+          nil))))
