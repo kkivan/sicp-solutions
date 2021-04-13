27f4a093c59b41bf47e6a01a451e96cbce0cd02e
Ivan Kvytakovskiy <ivan.kvyatkovski@gmail.com>
2021-04-13 16:36:51 +0700

    Fixes coercion

--- 2-81.scm
+++ 2-81.scm
@@ -12,3 +12,32 @@
 ;b. It will work with out same type coercion since coercion is done after we didn't hit the original table
 
 ;;c.
+
+(require "modules/sicp/sicp.rkt")
+(require "dispatch-table.scm")
+(require "modules/sicp/rat.scm")
+
+(define (** base exponent)
+  (fold-right mul 1 (repeated base exponent)))
+
+(define (exp x y) (apply-generic 'exp x y))
+(define (mul a b) (apply-generic 'mul a b))
+
+(put 'exp '(scheme-number scheme-number) **)
+(put 'mul '(scheme-number scheme-number) *)
+
+(put-coercion 'scheme-number 'rat (lambda (n)
+                                    (make-rat n 1)))
+
+(put 'mul '(rat rat) (lambda (l r)
+                       (make-rat (mul (numer l) (numer r))
+                                 (mul (denom l) (denom r)))))
+
+(define (make-complex-real-imag real imag)
+  (attach-tag 'complex (cons real imag)))
+
+(assert (mul 5 3)
+        15)
+
+(assert (exp 2 4)
+        16)
