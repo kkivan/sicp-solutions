2c979e6dd9164748bcb22b8b4a024835b48e076b
Ivan Kvytakovskiy <ivan.kvyatkovski@gmail.com>
2021-04-25 15:24:08 +0700

    Updates dense list tests

--- poly-dense-tests.scm
+++ poly-dense-tests.scm
@@ -3,41 +3,43 @@
 (require "modules/sicp/sicp.rkt")
 (require "dispatch-table.scm")
 (require "math.scm")
-(require "poly-dense.scm")
+(require "poly.scm")
+(require "dense-term-list.scm")
 (require "scheme-number.scm")
 
 (install-polynomial-package)
 (install-package-scheme-number)
+(install-dense-term-list-package)
 
 (define (run-poly-tests)
   (list 
-   (assert (variable (contents (make-poly 'x '(4 0 2 0))))
+   (assert (variable (contents (make-poly 'x (make-dense-term-list '(4 0 2 0)))))
            'x)
 
-   (assert (term-list (contents (make-poly 'x '(4 0 2 0))))
-           '(4 0 2 0))
+   (assert (term-list (contents (make-poly 'x (make-dense-term-list '(4 0 2 0)))))
+           (make-dense-term-list '(4 0 2 0)))
    
-   (assert (add (make-poly 'x '(0 4 0 2 0))
-                (make-poly 'x '(4 0 2 0)))
-           (make-poly 'x '(0 8 0 4 0)))
+   (assert (add (make-poly 'x (make-dense-term-list '(0 4 0 2 0)))
+                (make-poly 'x (make-dense-term-list '(4 0 2 0))))
+           (make-poly 'x (make-dense-term-list '(0 8 0 4 0))))
    
-   (assert (add (make-poly 'x '(4 0 2 0))
-                (make-poly 'x '(4 2 0 0)))
-           (make-poly 'x '(8 2 2 0)))
+   (assert (add (make-poly 'x (make-dense-term-list '(4 0 2 0)))
+                (make-poly 'x (make-dense-term-list '(4 2 0 0))))
+           (make-poly 'x (make-dense-term-list '(8 2 2 0))))
 
-   (assert (=zero? (make-poly 'x '(0 0 0 0)))
+   (assert (=zero? (make-poly 'x (make-dense-term-list '(0 0 0 0))))
            true)
    
-   (assert (negate (make-poly 'x '(4 0 2 0)))
-           (make-poly 'x '(-4 0 -2 0)))
+   (assert (negate (make-poly 'x (make-dense-term-list '(4 0 2 0))))
+           (make-poly 'x (make-dense-term-list '(-4 0 -2 0))))
 
-   (assert (sub (make-poly 'x '(4 0 2 0))
-                (make-poly 'x '(4 0 2 0)))
-           (make-poly 'x '(0 0 0 0)))
+   (assert (sub (make-poly 'x (make-dense-term-list '(4 0 2 0)))
+                (make-poly 'x (make-dense-term-list '(4 0 2 0))))
+           (make-poly 'x (make-dense-term-list '(0 0 0 0))))
 
-   (assert (sub (make-poly 'x '())
-                (make-poly 'x '(4 0 2 0)))
-           (make-poly 'x '(-4 0 -2 0)))
+   (assert (sub (make-poly 'x (make-dense-term-list '()))
+                (make-poly 'x (make-dense-term-list '(4 0 2 0))))
+           (make-poly 'x (make-dense-term-list '(-4 0 -2 0))))
    )
   )
    
