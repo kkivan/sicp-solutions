3f97485bf717ae103fd1add0e17a5e5de155ece1
Ivan Kvytakovskiy <ivan.kvyatkovski@gmail.com>
2021-10-31 18:07:20 +0700

    adds solution for 3-25

--- table.scm
+++ table.scm
@@ -6,17 +6,6 @@
   (mcar (mcar pair)))
 
 (define (make-table same-key?)
-  (define (lookup key-1 key-2 table)
-    (let ((subtable
-           (assoc key-1 (mcdr table))))
-      (if subtable
-          (let ((record
-                 (assoc key-2 (mcdr subtable))))
-            (if record
-                (mcdr record)
-                false))
-          false)))
-
   (define (assoc key records)
     (cond ((null? records) false)
           ((same-key? key (mcaar records)) (mcar records))
@@ -35,38 +24,55 @@
                                   (cons key-2 value))
                             (mcdr table)))))
     'ok)
+  
   (let ((local-table (mlist '*table*)))
-    (define (lookup key-1 key-2)
-      (let ((subtable
-             (assoc key-1 (mcdr local-table))))
-        (if subtable
-            (let ((record
-                   (assoc key-2 (mcdr subtable))))
-              (if record (mcdr record) false)) false)))
-    (define (insert! key-1 key-2 value)
-      (let ((subtable
-             (assoc key-1 (mcdr local-table))))
-        (if subtable
-            (let ((record
-                   (assoc key-2 (cdr subtable))))
-              (if record
-                  (set-mcdr! record value) (set-mcdr! subtable
-                                                      (mcons (mcons key-2 value)
-                                                             (mcdr subtable)))))
-            (set-mcdr! local-table
-                       (mcons (mlist key-1 (mcons key-2 value))
-                              (mcdr local-table)))))
-      'ok)
+
+    (define (lookup keys) 
+      (define (iter keys table) 
+        (let ((subtable (assoc (mcar keys) (mcdr table)))) 
+          (if subtable 
+              (cond ((null? (mcdr keys)) (mcdr subtable)) 
+                    (else (iter (mcdr keys) subtable))) 
+              false))) 
+      (iter keys local-table))
+    
+    (define (gen-new-list keys value) 
+         (if (null? (mcdr keys)) 
+             (mcons (mcar keys) value) 
+             (mlist (mcar keys) (gen-new-list (mcdr keys) value)))) 
+      
+     (define (insert! keys value) 
+       (define (iter keys table) 
+         (let ((subtable (assoc (mcar keys) (mcdr table)))) 
+           (if subtable 
+               (cond ((null? (mcdr keys)) 
+                      (set-mcdr! subtable value)) 
+                     (else 
+                      (iter (mcdr keys) subtable))) 
+               (set-mcdr! table (mcons (gen-new-list keys value) (mcdr table))))) 
+         'ok) 
+       (iter keys local-table)) 
+    
     (define (dispatch m)
       (cond ((eq? m 'lookup-proc) lookup)
             ((eq? m 'insert-proc!) insert!)
+            ((eq? m 'display) local-table)
             (else (error "Unknown operation: TABLE" m))))
     dispatch))
 
 (define t (make-table equal?))
 
-(assert ((t 'insert-proc!) 'cities 'bangkok 333)
+(assert ((t 'insert-proc!) (mlist 'cities 'bangkok) 333)
         'ok)
-(assert ((t 'lookup-proc) 'cities 'bangkok)
+
+(assert ((t 'insert-proc!) (mlist 'cities 'tokyo) 111)
+        'ok)
+
+(t 'display)
+
+(assert ((t 'lookup-proc) (mlist 'cities 'bangkok))
         333)
+
+(assert ((t 'lookup-proc) (mlist 'cities 'tokyo))
+        111)
 
