e9f1b47ee4df02ea6f235286a37e886d829f9633
Ivan Kvytakovskiy <ivan.kvyatkovski@gmail.com>
2021-12-31 12:11:09 +0700

    adds squarer, solution for 3-35

--- constraints-programming.scm
+++ constraints-programming.scm
@@ -210,3 +210,40 @@
 
 (set-value! A 2 'user)
 (set-value! S 3 'user)
+
+
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+        (if (< (get-value b) 0)
+            (error "square less than 0: SQUARER"
+                   (get-value b))
+            (set-value! a (sqrt (get-value b)) me))
+        (if (has-value? a)
+            (set-value! b (* (get-value a)
+                             (get-value a)) me)
+            (error "Both a and b aren't set"))))
+  
+  (define (process-forget-value)
+    (forget-value! a me)
+    (forget-value! b me)
+    (process-new-value))
+  
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value) (process-new-value))
+          ((eq? request 'I-lost-my-value) (process-forget-value))
+          (else (error "Unknown request: MULTIPLIER"
+                       request))))
+  (connect a me)
+  (connect b me)
+  me)
+
+(define X (make-connector))
+(define Y (make-connector))
+
+(probe "X: " X)
+(probe "Y: " Y)
+
+(squarer X Y)
+
+(set-value! X 2 'user)
