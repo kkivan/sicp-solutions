8f13771e02fb5a66a4096b65ecdd867bc02b3433
Ivan Kvytakovskiy <ivan.kvyatkovski@gmail.com>
2022-01-08 19:06:22 +0700

    adds solution for 3-65

--- 3-59.scm
+++ 3-59.scm
@@ -41,4 +41,27 @@
       (mul-series num
                   (invert-unit-series den))))
 
-(define tangent-series (div-series sine-series cosine-series))                            
+(define tangent-series (div-series sine-series cosine-series))
+
+
+(define (partial-sums s) 
+   (add-streams s (cons-stream 0 (partial-sums s)))) 
+  
+(define (pi-summands n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (pi-summands (+ n 2)))))
+
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1)) 4))
+
+;(take pi-stream 1000)
+
+(take (pi-summands 1) 10)
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+        (s1 (stream-ref s 1))
+        (s2 (stream-ref s 2)))
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
