39c174fe5d2432fe7f3aa3c7e50f8dddf67b1b75
Ivan Kvytakovskiy <ivan.kvyatkovski@gmail.com>
2022-05-04 18:41:38 +0700

    updates solution

--- 3-81.scm
+++ 3-81.scm
@@ -3,12 +3,12 @@
 (require "streams.scm")
 (require "modules/sicp/sicp.rkt")
 
-(define (random-nums requests initial)
+(define (random-nums requests seed)
   (define (random-update prev)
     (remainder (+ (* 13 prev) 5) 24))
   (let ((request (stream-car requests)))
-    (cond ((eq? (car request) 'generate) (cons-stream (random-update initial)
-                                                      (random-nums (stream-cdr requests) (random-update initial))))
+    (cond ((eq? (car request) 'generate) (cons-stream (random-update seed)
+                                                      (random-nums (stream-cdr requests) (random-update seed))))
           ((eq? (car request) 'reset) (random-nums (stream-cdr requests) (cdr request)))
           (else "Bad request"))))
 
@@ -17,11 +17,10 @@
                (cons-stream (cons 'generate nil)
                             (cons-stream (cons 'reset 5)
                                          (cons-stream (cons 'generate nil)
-                                                      the-empty-stream)))))
+                                                      req-stream)))))
 
 (take (random-nums req-stream 1) 3)
 (take (random-nums req-stream 2) 3)
-(take (random-nums req-stream 3) 3)
-
+(take (random-nums req-stream 3) 11)
 
               
